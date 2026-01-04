669939e169d2176868dc22474864ce34491fdace
Sangam Thapa <t.sangam2a88@gmail.com>
2026-01-04 10:48:21 -0600

    Time: 454 ms (89.95%), Space: 0B (100%) - LeetHub

--- 1179-reformat-department-table/1179-reformat-department-table.sql
+++ 1179-reformat-department-table/1179-reformat-department-table.sql
@@ -1,17 +1,16 @@
 # Write your MySQL query statement below
-select 
-  id,
-  sum(case when month = 'Jan' then revenue end) as Jan_Revenue,
-  sum(case when month = 'Feb' then revenue end) as Feb_Revenue,
-  sum(case when month = 'Mar' then revenue end) as Mar_Revenue,
-  sum(case when month = 'Apr' then revenue end) as Apr_Revenue,
-  sum(case when month = 'May' then revenue end) as May_Revenue,
-  sum(case when month = 'Jun' then revenue end) as Jun_Revenue,
-  sum(case when month = 'Jul' then revenue end) as Jul_Revenue,
-  sum(case when month = 'Aug' then revenue end) as Aug_Revenue,
-  sum(case when month = 'Sep' then revenue end) as Sep_Revenue,
-  sum(case when month = 'Oct' then revenue end) as Oct_Revenue,
-  sum(case when month = 'Nov' then revenue end) as Nov_Revenue,
-  sum(case when month = 'Dec' then revenue end) as Dec_Revenue
+select id, 
+    max(case when Department.month = 'Jan' then revenue end) as Jan_Revenue,
+    max(case when Department.month = 'Feb' then revenue end) as Feb_Revenue,
+    max(case when Department.month = 'Mar' then revenue end) as Mar_Revenue,
+    max(case when Department.month = 'Apr' then revenue end) as Apr_Revenue,
+    max(case when Department.month = 'May' then revenue end) as May_Revenue,
+    max(case when Department.month = 'Jun' then revenue end) as Jun_Revenue,
+    max(case when Department.month = 'Jul' then revenue end) as Jul_Revenue,
+    max(case when Department.month = 'Aug' then revenue end) as Aug_Revenue,
+    max(case when Department.month = 'Sep' then revenue end) as Sep_Revenue,
+    max(case when Department.month = 'Oct' then revenue end) as Oct_Revenue,
+    max(case when Department.month = 'Nov' then revenue end) as Nov_Revenue,
+    max(case when Department.month = 'Dec' then revenue end) as Dec_Revenue 
 from Department
-group by id;
+group by id
