22a72d84028c8e1a1b1dc78f11db266bbf1eadbc
Sangam Thapa <t.sangam2a88@gmail.com>
2026-01-06 01:55:42 -0600

    Time: 538 ms (25.37%), Space: 0B (100%) - LeetHub

---
+++ 3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.sql
@@ -0,0 +1,16 @@
+
+select 
+  l.book_id,
+  l.title,
+  l.author,
+  l.genre,
+  l.publication_year,
+  l.total_copies as current_borrowers
+
+
+from library_books l
+join borrowing_records b
+on l.book_id = b.book_id
+group by b.book_id
+having (l.total_copies - count(case when b.return_date is null then 1 end)) = 0
+order by current_borrowers desc, l.title asc
