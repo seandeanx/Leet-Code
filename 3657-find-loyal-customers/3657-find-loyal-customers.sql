b54f9e124891772ad6b53d484aaec1d96f01468b
Sangam Thapa <t.sangam2a88@gmail.com>
2026-01-21 13:48:46 -0600

    Time: 3379 ms (5.04%), Space: 0B (100%) - LeetHub

--- 3657-find-loyal-customers/3657-find-loyal-customers.sql
+++ 3657-find-loyal-customers/3657-find-loyal-customers.sql
@@ -1,13 +1,21 @@
+-- # Write your MySQL query statement below
+-- select customer_id
+-- from customer_transactions
+-- group by customer_id
+-- having
+--     sum(
+--         case 
+--             when transaction_type = 'purchase' then 1 else 0 end) >= 3
+--         and datediff(max(transaction_date), min(transaction_date)) >= 30
+--         and sum(
+--                 case 
+--                     when transaction_type = 'refund' then 1 else 0 end) / count(*) < 0.2
+-- order by customer_id;
+
 # Write your MySQL query statement below
-select customer_id
+select customer_id      
 from customer_transactions
 group by customer_id
-having
-    sum(
-        case 
-            when transaction_type = 'purchase' then 1 else 0 end) >= 3
-        and datediff(max(transaction_date), min(transaction_date)) >= 30
-        and sum(
-                case 
-                    when transaction_type = 'refund' then 1 else 0 end) / count(*) < 0.2
-order by customer_id;
+having sum(transaction_type ='purchase') >= 3 
+and avg(transaction_type ='refund') < 0.2
+and DATEDIFF(max(transaction_date), min(transaction_date)) >= 30
